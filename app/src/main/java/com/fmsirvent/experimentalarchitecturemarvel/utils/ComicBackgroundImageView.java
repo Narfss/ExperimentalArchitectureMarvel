733cc5f3d153bb923377e6c09d71295a25e9194a
Narf <narfss@gmail.com>
2016-10-02 17:46:31 +0200

    Improved comics background animation

--- app/src/main/java/com/fmsirvent/experimentalarchitecturemarvel/utils/ComicBackgroundImageView.java
+++ app/src/main/java/com/fmsirvent/experimentalarchitecturemarvel/utils/ComicBackgroundImageView.java
@@ -3,17 +3,22 @@ package com.fmsirvent.experimentalarchitecturemarvel.utils;
 import android.content.Context;
 import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
-import com.fmsirvent.experimentalarchitecturemarvel.R;
 import com.fmsirvent.experimentalarchitecturemarvel.view.images.MarvelImageMVO;
 
 import java.util.List;
 
-public class ComicBackgroundImageView extends AppCompatImageView {
-    public static final int DELAY_MILLIS = 10000;
+public class ComicBackgroundImageView extends FrameLayout {
+    public static final int DELAY_MILLIS = 3000;
     private int currentImage;
     private List<MarvelImageMVO> images;
     private Runnable runnable;
+    private Runnable frontRunnable;
+    private AppCompatImageView backgroundImage;
+    private AppCompatImageView overBackgroundImage;
 
     public ComicBackgroundImageView(Context context) {
         super(context);
@@ -30,15 +35,22 @@ public class ComicBackgroundImageView extends AppCompatImageView {
         init(context, attrs, defStyleAttr);
     }
 
-    private void init(Context context, AttributeSet attrs, int defStyleAttr) { }
+    private void init(Context context, AttributeSet attrs, int defStyleAttr) {
+        backgroundImage = new AppCompatImageView(context);
+        LayoutParams params = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        backgroundImage.setLayoutParams(params);
+        backgroundImage.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        addView(backgroundImage);
+        overBackgroundImage = new AppCompatImageView(context);
+        overBackgroundImage.setLayoutParams(params);
+        overBackgroundImage.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        addView(overBackgroundImage);
+    }
 
     public void renderImages(final List<MarvelImageMVO> images) {
         this.images = images;
         currentImage = 0;
         loadCurrentImage();
-        if (images.size() > 1) {
-            postDelayedLoadMore();
-        }
     }
 
     private void postDelayedLoadMore() {
@@ -46,26 +58,48 @@ public class ComicBackgroundImageView extends AppCompatImageView {
             runnable = new Runnable() {
                 @Override
                 public void run() {
-                    increaseCircularCurrentImage();
-                    loadCurrentImage();
+                    loadCurrentImageInBackground();
+                    postDelayLoadOverBackground();
                 }
             };
         }
         postDelayed(runnable, DELAY_MILLIS);
     }
 
+    private void postDelayLoadOverBackground() {
+        if (frontRunnable == null) {
+            frontRunnable = new Runnable() {
+                @Override
+                public void run() {
+                    increaseCircularCurrentImage();
+                    loadCurrentImage();
+                }
+            };
+        }
+        postDelayed(frontRunnable, DELAY_MILLIS);
+    }
+
     private void increaseCircularCurrentImage() {
         currentImage++;
-        if (currentImage > images.size()) {
+        if (currentImage >= images.size()) {
             currentImage = 0;
+        }
+    }
+
+    private void loadCurrentImageInBackground() {
+        if (currentImage < images.size()) {
+            String imageURL = images.get(currentImage).getURL(MarvelImageMVO.Original.DETAIL);
+            ImageLoader.loadImage(backgroundImage, imageURL);
         }
     }
 
     private void loadCurrentImage() {
         if (currentImage < images.size()) {
             String imageURL = images.get(currentImage).getURL(MarvelImageMVO.Original.DETAIL);
-            ImageLoader.loadImageFade(this, imageURL);
-            postDelayedLoadMore();
+            ImageLoader.loadImageFade(overBackgroundImage, imageURL);
+            if (images.size() > 1) {
+                postDelayedLoadMore();
+            }
         }
     }
 
@@ -73,6 +107,7 @@ public class ComicBackgroundImageView extends AppCompatImageView {
     protected void onDetachedFromWindow() {
         if (runnable != null) {
             removeCallbacks(runnable);
+            removeCallbacks(frontRunnable);
         }
         super.onDetachedFromWindow();
     }
--- app/src/main/java/com/fmsirvent/experimentalarchitecturemarvel/utils/ImageLoader.java
+++ app/src/main/java/com/fmsirvent/experimentalarchitecturemarvel/utils/ImageLoader.java
@@ -1,15 +1,24 @@
 package com.fmsirvent.experimentalarchitecturemarvel.utils;
 
 import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.graphics.Palette;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.Request;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.animation.GlideAnimation;
 import com.bumptech.glide.request.target.BitmapImageViewTarget;
+import com.bumptech.glide.request.target.SizeReadyCallback;
+import com.bumptech.glide.request.target.Target;
 
 public class ImageLoader {
+    public static final int FADE_DURATION = 2000;
+
     public static void loadImage(ImageView imageView, String imageURL) {
         Glide.with(imageView.getContext())
                 .load(imageURL)
@@ -19,7 +28,8 @@ public class ImageLoader {
     public static void loadImageFade(ImageView imageView, String imageURL) {
         Glide.with(imageView.getContext())
              .load(imageURL)
-             .crossFade()
+             .crossFade(FADE_DURATION)
+             .skipMemoryCache(true)
              .into(imageView);
     }
 
